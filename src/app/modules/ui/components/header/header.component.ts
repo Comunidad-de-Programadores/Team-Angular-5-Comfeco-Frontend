bed0ef58681624d897af85c2733baa839f2cd9ba
Geferman Pernia <geferman.pro@gmail.com>
2021-02-16 03:57:49 -0400

    feat (auth.guard.ts): desarrollo de guard #8

--- src/app/modules/ui/components/header/header.component.ts
+++ src/app/modules/ui/components/header/header.component.ts
@@ -10,6 +10,10 @@ export class HeaderComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const path = this.location.path();
+    if (path === '/account') {
+      this.isActived = true;
+    }
   }
 
 }
