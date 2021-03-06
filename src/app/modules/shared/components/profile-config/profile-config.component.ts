0e04741e512130d27064b420e31ea3ec49084223
iamrivard <erick.sgr10@gmail.com>
2021-03-05 19:45:58 -0600

    feat(configuracion): maquetacion de configuracion de usuario #45

---
+++ src/app/modules/shared/components/profile-config/profile-config.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-config',
+  templateUrl: './profile-config.component.html',
+  styleUrls: ['./profile-config.component.scss']
+})
+export class ProfileConfigComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
